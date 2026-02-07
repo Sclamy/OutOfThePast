57db3f0eb83294bd0c750531b8fa7580b8df95f8
Sclamy <sclamysteam@gmail.com>
2026-02-07 13:05:11 -0500

    Added a few more files

---
+++ Patches/SideJobPatches.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using SOD.Common;
+// using SOD.Common.Extensions;
+
+
+namespace OutOfThePast.Patches
+{
+    internal static class SideJobPatches
+    {
+        /*
+        [HarmonyPatch(typeof(FirstPersonItemController), "Update")] // runs after a bunch of data is loaded
+        internal static class PatchUpdate
+        {
+            [HarmonyPostfix]
+            static void Postfix(FirstPersonItemController __instance)
+            {
+            }
+        }
+        */
+        
+        
+        
+    }
+}
